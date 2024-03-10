03169c893477cc4cfae2c01b1cf7187a673cb844
Siddhant <siddhsakhare1@gmail.com>
2024-03-10 12:24:34 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+
+st.set_page_config(layout='wide')
+
+df = pd.read_csv('C:/Users/siddhant sakhare/Documents/Plotly/india.csv')
