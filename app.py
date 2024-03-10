d100cb03a071d38cc691116cde0840a502a2066b
Siddhant <siddhsakhare1@gmail.com>
2024-03-10 12:43:44 +0530

    Data Visualize

--- app.py
+++ app.py
@@ -7,4 +7,8 @@ st.set_page_config(layout='wide')
 
 df = pd.read_csv('C:/Users/siddhant sakhare/Documents/Plotly/india.csv')
 
+list_of_state = list(df['State'].unique())
+list_of_state.insert(0, 'Overall India')
+
 st.sidebar.title = ('India ka Data Visualization')
+st.sidebar.selectbox('Select a State', list_of_state)
