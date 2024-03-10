5fc52cd56e98d4a712f4eb9fbd302bebf75b1c21
Siddhant <siddhsakhare1@gmail.com>
2024-03-10 12:38:29 +0530

    Selecting state commit

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ import plotly.express as px
 st.set_page_config(layout='wide')
 
 df = pd.read_csv('C:/Users/siddhant sakhare/Documents/Plotly/india.csv')
+
+st.sidebar.title = ('India ka Data Visualization')
